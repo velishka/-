be09b937920f981863efc45e52d5e8f9a83f4f6a
velishka <112897094+velishka@users.noreply.github.com>
2025-03-30 16:58:33 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("loveButton").addEventListener("click", function() {
+    let message = document.getElementById("message");
+    message.classList.remove("hidden");
+    message.style.opacity = "1";
+});
